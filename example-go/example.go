b5220f3e25d54dbcc9c8ed786d037fc4a2c04277
SethWickham <seth.wickham@hotmail.com>
2019-03-25 18:42:06 -0700

    added some basic go funcionality including mapping and conversion

---
+++ example-go/example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Go Pillars Efficient Execution, Efficient compilation, and ease of programming
+	// an example of a Multi Dimensional Slice
+	iceCream := []string{"Chocolate", "Vanilla", "Strawberry"}
+	fmt.Println("first string slice:", iceCream)
+	numberString := []string{"32", "64", "8"}
+	fmt.Println("second string slice:", numberString)
+	// slice of slice xi
+	xi := [][]string{iceCream, numberString}
+	fmt.Println("slice of a slice:", xi)
+	// Mapping
+	m := map[string]int{
+		"Seth":    32,
+		"ShirtId": 4567,
+	}
+	fmt.Println(m)
+	fmt.Println(m["Seth"])
+	fmt.Println(m["Jeans"])
+
+	if v, ok := m["Jeans"]; ok {
+		fmt.Println("THIS IS THE IF PRINT FOR A VALUE THAT DOESN't EXIST, Value:", v)
+	} else if v, ok := m["Seth"]; ok {
+		fmt.Println("THIS IS THE IF PRINT FOR A VALUE THAT EXISTS, Value:", v)
+
+		// Pulling in a method on a seperate file but in the same package looks like this:
+		other()
+
+		//My name in binary using conversion to convert a string into slice of byte
+		x := "Seth Wickham"
+		nx := []byte(x)
+		fmt.Println("My name normally:", x)
+
+		fmt.Printf("If a computer called out my name with no translator I would hear this:%b\n", nx)
+
+	}
+}
